dbabb12f10bdd8a03054b89f93f59b1df71e42c3
A Saifur R <asrsaif04@gmail.com>
2023-03-05 11:27:57 +0700

    section 11 update prioritas 1-4

--- 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/prioritas 1/prioritas1-4.go
+++ 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/prioritas 1/prioritas1-4.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
-	// "math"
+	"math"
+	// "sort"
 )
-func simpleEquations(a,b,c int)  {
-	// for x := int(math.Sqrt(float64(c))); x <= a/3; x++ {
-	// 	for y := x; y <= (a-x)/2; y++ {
-	// 		z := a - x - y
-	// 		if x*y*z == b && x*x+y*y+z*z == c {
-	// 			fmt.Print(x, y, z)
-	// 		}
-	// 	}
-	// }
-	fmt.Println("no solution")
+
+func simpleEquations(a, b, c int){
+    x, y, z := 0, 0, 0
+	found:=false
+    for i := 1; i <= int(math.Sqrt(float64(b))); i++ {
+        if b % i == 0 {
+            j := b / i
+            for k := -int(math.Sqrt(float64(c))); k <= int(math.Sqrt(float64(c))); k++ {
+                if i != j && i != k && j != k && i + j + k == a && i * j * k == b && i*i + j*j + k*k == c {
+                    x = i
+                    y = j
+                    z = k
+					found = true
+                }
+            }
+        }
+    }
+	if found==true {
+		fmt.Println(x,y,z)
+	}else{
+		fmt.Println("no solution")
+	}
 }
+
 func main() {
-	simpleEquations(1, 2, 3)
-	simpleEquations(6, 6, 14)
+   simpleEquations(1,2,3)
+   simpleEquations(6,6,14)
 }
